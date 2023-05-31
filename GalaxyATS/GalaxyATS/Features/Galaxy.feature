ee45076d469df1030a64a3bbcf03fa540cb106aa
jayan4537 <Jayanta.Dutta@towerswatson.com>
2023-05-31 13:42:33 +0530

    COmpleted 1 database scenario

--- GalaxyATS/GalaxyATS/Features/Galaxy.feature
+++ GalaxyATS/GalaxyATS/Features/Galaxy.feature
@@ -24,17 +24,18 @@ Scenario: Validate the active campaigns and all the campaign segments of partner
 	| Ford Medicare - Salary (Losing Group Medical and Prescription Drug Coverage)                                 |
 	| Ford Medicare Age-Ins: No Current Group Coverage (turning age 65)                                            |
 	And I validate the following campaignsegments of the campaign "Ford Pre-Medicare hired before 6/1/01"
+	| CampaignSegmentName                                                                                          |
 	| Ford Individual and Family Plan Eligible (Losing Group Medical and Prescription Drug Coverage)               |
 	| Ford Individual and Family Plan Eligible (Waived, No Current Group Coverage)                                 |
 	| Ford Pre-Medicare hired before 6/1/01                                                                        |
 	And I validate the following campaignsegments of the campaign "Ford Medicare hired after 6/1/01"
+	| CampaignSegmentName                                                                                          |
 	| Ford Medicare post 6/1/01 (Losing Group Medical and Prescription Drug Coverage)                              |
 	| Ford Medicare post 6/1/01 (Losing Vision Access Only)                                                        |
 	| Ford Medicare post 6/1/01 (Waived/Deffered, No Current Group Coverage)                                       |
-	And I validate the following campaignsegments of the campaign "Ford Medicare hired after 6/1/01"
+	And I validate the following campaignsegments of the campaign "Ford Pre-Medicare hired after 6/1/01"
+	| CampaignSegmentName                                                                                          |
 	| Ford Individual and Family Plan Eligible — post 6/1/01 (Losing Group Medical and Prescription Drug Coverage) |
 	| Ford Individual and Family Plan Eligible — post 6/1/01 (Losing Vision)                                       |
 	| Ford Individual and Family Plan Eligible — post 6/1/01 (Waived/Deffered, No Current Group Coverage)          |
 	And I validate there are no campaignsegments for the campaign "Ford IMM"
-
-
--- GalaxyATS/GalaxyATS/StepDefinitions/GalaxyConnectivityStepDefinitions.cs
+++ GalaxyATS/GalaxyATS/StepDefinitions/GalaxyConnectivityStepDefinitions.cs
@@ -24,46 +24,7 @@ namespace GalaxyATS.StepDefinitions
             }
            catch (Exception ex) { Assert.Fail("Failed to connect Database"); }
         }
-        [Then(@"I check the existance of the ""([^""]*)"" in the ""([^""]*)"" table")]
-       
-        public void ThenICheckTheExistanceOfThePartnerAtTheTable(string value, string tablename,Table tab)
-        {
-            var searchparam = "partnerId";
-            var query = "select " + searchparam + " from " + tablename+" where partnerName='"+value+"'";
-            DataTable dt=DBTableOps.getdatafromQuery(query);
-            partnerid = tab.Rows[0]["partnerid"];
-            string expected_partnerID = dt.Rows[0][searchparam].ToString();
-            expected_partnerID.Should().Be(partnerid);
-            ScenarioContext.Current["partnerId"] = expected_partnerID;
-
-
-        }
-        [When(@"I find the partner is existing in the system")]
-        public void WhenIFindThePartnerIsExistingInTheSystem()
-        {
-            Console.WriteLine("PartnerID is : "+ScenarioContext.Current["partnerId"]);
-        }
-        [Then(@"I validate the following campaigns are available for the partner")]
-        public void ThenIValidateTheFollowingCampaignsAreAvailableForThePartner(Table table)
-        {
-            var query = "select campaignId,campaignName from dbo.campaign where partnerid="+ ScenarioContext.Current["partnerId"];
-            DataTable dt = DBTableOps.getdatafromQuery(query);
-            for(int i=0; i<dt.Rows.Count; i++)
-            {
-                var actual_campaignid = dt.Rows[i]["campaignId"].ToString();
-                var expected_campaignId=table.Rows[i]["campaignId"].ToString();
-                expected_campaignId.Should().Be(actual_campaignid);
-                var actual_campaignname = dt.Rows[i]["campaignName"].ToString(); ;
-                var expected_campaignname = table.Rows[i]["campaignName"].ToString(); ;
-                expected_campaignname.Should().Be(actual_campaignname);
-
-            }
-        }
-        [Then(@"I validate the following campaignsegments of the campaign ""([^""]*)""")]
-        public void ThenIValidateTheFollowingCampaignsegmentsOfTheCampaign(string p0, Table table)
-        {
-            
-        }
+        
 
 
 
